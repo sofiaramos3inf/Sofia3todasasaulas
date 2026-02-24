e84b2e1dbff2bd0df9d3c5d52322290db0ad4b70
sofiramoss <sofia2inf@gmail.com>
2026-02-24 13:57:17 -0300

    cgfn

---
+++ php.myslq/index.php
@@ -0,0 +1,12 @@
+<?php
+//comandos de saída
+//echo
+echo "<h1>Olá mundo!<h1><br>
+<h3>Hoje está chovendo muito!!<h3>";
+
+//print
+echo "<br>--------------<br>";
+$mensagem = "Olá!";
+if (print $mensagem) {
+ echo "-Mensagem exibida com sucesso!";
+}
