ae27d1cf80f5fb4979b6b519eb20e9e007703bbc
sofiramoss <sofia2inf@gmail.com>
2026-03-02 12:18:08 -0300

    sdfg
    drg

--- php.myslq/index.php
+++ php.myslq/index.php
@@ -1,12 +1,18 @@
 <?php
-//comandos de saída
-//echo
-echo "<h1>Olá mundo!<h1><br>
-<h3>Hoje está chovendo muito!!<h3>";
+$nome = "Sofia";
+$idade = 17;
+$altura = 1.70;
+$address = "Rua dos bobos, 0";
+echo "Nome: $nome, Altura: $altura, Idade: $idade, Endereço: $address <br>";
+echo "Nome: $nome, Idade: $idade, Endereço: $address <br>";
 
-//print
-echo "<br>--------------<br>";
-$mensagem = "Olá!";
-if (print $mensagem) {
- echo "-Mensagem exibida com sucesso!";
+//Exercicio 1
+
+$idade = 17;
+if ($idade >= "18"){
+    echo "Você já é maior de idade, pode tirar sua cartira!";
 }
+else if ($idade < "18");{
+    echo "Você é menor de idade, não pode tirar sua cartira e vá estudar!";}
+
+//Exercício 2
